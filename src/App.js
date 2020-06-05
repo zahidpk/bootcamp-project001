b58166a5a99f9e462b8e83cb5c56306408919281
Zahid Iqbal <zahidpugc@gmail.com>
2020-06-06 02:15:55 +0500

    Update node action version

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        Hello World
+        <h1>Hello World</h1>
       </header>
     </div>
   );
